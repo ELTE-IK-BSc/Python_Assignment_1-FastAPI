a785b221e499eef4c92ae94a199ea23503880fdb
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-08 23:48:58 +0200

    Add read_events_from_file function

--- app/src/file_storage.py
+++ app/src/file_storage.py
@@ -7,4 +7,13 @@ class EventFileManager:
     def __init__(self):
         self.FILE_PATH = "event.json"
 
+    def read_events_from_file(self) -> list[Event]:
+        event_list = []
+        with open(self.FILE_PATH, "r") as read_file:
+            data = json.load(read_file)
+            for event_data in data:
+                event = Event(**event_data)
+                event_list.append(event)
+        return event_list
+
 
