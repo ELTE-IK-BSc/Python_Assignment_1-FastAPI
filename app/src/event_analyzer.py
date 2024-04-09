db4c84843158bf7b5ffca441bfd1f50ccb773b53
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-09 18:49:38 +0200

    Add get_joiners_multiple_meetings_method

--- app/src/event_analyzer.py
+++ app/src/event_analyzer.py
@@ -1,1 +1,14 @@
+from .models import Event
+
+
 class EventAnalyzer:
+    def get_joiners_multiple_meetings_method(events: list[Event]):
+        allJoiners = []
+        multiJoiners = []
+        for event in events:
+            for joiner in event.joiners:
+                if joiner in allJoiners:
+                    multiJoiners.append(joiner)
+                else:
+                    allJoiners.append(joiner)
+        return multiJoiners
